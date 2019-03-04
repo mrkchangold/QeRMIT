7f764fc1fcc5ffaac641a85e39b28cbf33c0a4dc
mrkchang <mrkchang@stanford.edu>
2019-03-04 02:53:59 -0800

    dbging

--- pytorch-pretrained-BERT/pytorch_pretrained_bert/modeling.py
+++ pytorch-pretrained-BERT/pytorch_pretrained_bert/modeling.py
@@ -1244,7 +1244,11 @@ class BertForQuestionAnswering(BertPreTrainedModel):
         batch, seq_len, hidden_dim = sequence_output.size()
         batch, seq_len = token_type_ids_flipped.size()
         token_type_ids_flipped = torch.unsqueeze(token_type_ids_flipped,2)
-        sequence_output_masked = torch.mul(sequence_output,token_type_ids_flipped.expand(-1,-1,hidden_dim))
+        print("DBG")
+        print(token_type_ids_flipped.size())
+        token_type_ids_flipped = token_type_ids_flipped.expand(-1,-1,hidden_dim)
+        print(token_type_ids_flipped.size())
+        sequence_output_masked = torch.mul(sequence_output,token_type_ids_flipped)
         if crop:
             sequence_output_masked = sequence_output_masked[:,:torch.max(query_length),:]
         print(sequence_output_masked.size())
