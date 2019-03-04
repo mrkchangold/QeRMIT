9c60b40169bc507c00a2f7de70a4ca4ac4ff0fc7
mrkchang <mrkchang@stanford.edu>
2019-03-04 11:33:59 -0800

    dbging

--- pytorch-pretrained-BERT/cnn_embedding.py
+++ pytorch-pretrained-BERT/cnn_embedding.py
@@ -111,11 +111,17 @@ class QEmbeddings(nn.Module):
     def forward(self, input):
         """
         """
+        print("INSIDE QEMBEDDING")
+        print(input.is_leaf)
         # batch, max_q_length, e_hidden = input.size()
         output = input.permute(0,2,1) #batch x e_hidden x max_q_length
+        print(output.is_leaf)
         output = self.cnn(output) #input: batch x e_hidden x max_q_length
+        print(output.is_leaf)
         output = output.permute(0,2,1) #input: batch x e_hidden x max_q_length
+        print(output.is_leaf)
         output = self.hwy(output) #input: batch x max_q_length x e_hidden
+        print(output.is_leaf)
         # output = output.view(sentence_length, batch_size, -1) # This seems unnecessary
         # print("OUTPUT SHAPE")
         # print(output.size()) #assuming batch x 1 x e_hidden
